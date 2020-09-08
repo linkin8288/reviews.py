fb8347aa485c9d2c7f8722db09cb8917d74bd9a1
Charles <linkin8288@gmail.com>
2020-09-08 10:44:20 +0200

    finding less than 100 data

--- reviews.py
+++ reviews.py
@@ -13,6 +13,14 @@ print('檔案讀取玩了，總共有', len(data), '筆資料') # 讀取長度
 # 找每筆留言的平均長度
 # 每筆資料就叫做d
 sum_len = 0 # 總留言長度 = 0
-for d in data:
+for d in data: # 將每筆資料一筆一筆叫出來
 	sum_len = sum_len + len(d) # 將目前的長度加上資料的長度，再存回目前的長度
 print('每筆留言的平均長度是', sum_len/len(data)) # 每筆留言的平均長度
+
+# 查有幾筆留言長度小於100
+new = [] # 新增空清單
+for d in data: # 將每筆資料一筆一筆叫出來
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0]) #將第一筆長度小於100的資料印出來
