db1bec3e1e91c0665fe5f40cbfeae4ead8497bc2
Charles <linkin8288@gmail.com>
2020-09-08 10:52:07 +0200

    finding target words in data

--- reviews.py
+++ reviews.py
@@ -8,7 +8,7 @@ with open('reviews.txt', 'r') as f: # 讀取txt檔案當作f
 		if count % 100000 == 0: # 每讀100000筆資料印出來，%是求餘數
 			print(len(data))
 
-print('檔案讀取玩了，總共有', len(data), '筆資料') # 讀取長度
+print('檔案讀取玩了，總共有', len(data), '筆資料') # len()是讀取長度
 
 # 找每筆留言的平均長度
 # 每筆資料就叫做d
@@ -24,3 +24,11 @@ for d in data: # 將每筆資料一筆一筆叫出來
 		new.append(d)
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0]) #將第一筆長度小於100的資料印出來
+
+# 找有good的留言
+nice = []
+for d in data:
+	if 'nice' in d:
+		nice.append(d) # 將此留言加入nice清單裡面
+print('一共有', len(nice), '筆留言提到nice')
+print(nice[0])
